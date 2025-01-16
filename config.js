f2da01bd643403dacee5cc24bca491e80aec3437
Rolling254 <jordanpope211@gmail.com>
2025-01-16 21:32:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-	SESSION_ID: process.env.SESSION_ID || '',
+	SESSION_ID: process.env.SESSION_ID || 'TUexTw46qYcLaxgQc5AAS6H7ZCpQFp17tc',
 	SUDO: process.env.SUDO || '',
 	API_ID: process.env.API_ID || 'https://xstro-api-4fb28ece11a9.herokuapp.com',
 	BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
 	STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;χѕтяσ мυℓтι ∂єνι¢є вσт',
 	WARN_COUNT: process.env.WARN_COUNT || 3,
-	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
+	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Nairobi,
 	DEBUG: process.env.DEBUG || false,
 	VERSION: '1.2.3'
 };
